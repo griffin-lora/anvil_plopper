a4442944b34474a97b1f5f5a55aa5aa3f5d783db
griffin-lora <36275551+griffin-lora@users.noreply.github.com>
2023-12-21 12:45:50 -0500

    update regions

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ use fastnbt::{Value};
 use fastnbt::from_bytes;
 use fastnbt::to_bytes;
 use std::fs::File;
-
-//
-// This loads a region file, extracts a chunk from it, and uses serde to
-// deserialize it into a `anvil::Chunk` object and print it.
-//
+use std::collections::HashMap;
 
 fn main() {
-    let file: File = File::options().read(true).write(true).open("regions/vanilla.mca").unwrap();
+    let file: File = File::options().read(true).write(true).open("regions/upper.mca").unwrap();
 
     let mut region: Region<File> = Region::from_stream(file).unwrap();
     let data = region.read_chunk(0, 0).unwrap().unwrap();
 
-    let chunk: Value = from_bytes(data.as_slice()).unwrap();
+    let chunk: HashMap<String, Value> = from_bytes(data.as_slice()).unwrap();
+
+    match chunk["DataVersion"] {
+        Value::Int(ver) => println!("Version: {}", ver),
+        _ => {},
+    }
 
     let bytes = to_bytes(&chunk).unwrap();
 
