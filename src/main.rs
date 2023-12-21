3f2f90a370a97990c6a9895dd33dd6bbbb8e5f67
griffin-lora <36275551+griffin-lora@users.noreply.github.com>
2023-12-21 14:31:15 -0500

    replace palette data

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,22 @@ use std::fs::File;
 use std::collections::HashMap;
 
 fn main() {
-    let file: File = File::options().read(true).write(true).open("regions/upper.mca").unwrap();
+    let file: File = File::options().read(true).write(true).open("regions/vanilla.mca").unwrap();
 
     let mut region: Region<File> = Region::from_stream(file).unwrap();
     let data = region.read_chunk(0, 0).unwrap().unwrap();
 
-    let chunk: HashMap<String, Value> = from_bytes(data.as_slice()).unwrap();
+    let mut chunk: HashMap<String, Value> = from_bytes(data.as_slice()).unwrap();
 
-    let sections: &Vec<Value> = match &chunk["sections"] {
+    let sections: &mut Vec<Value> = match chunk.get_mut("sections").unwrap() {
         Value::List(list) => list,
         _ => panic!("Failed to get sections")
     };
 
     println!("Num sections: {:?}", sections.len());
 
-    for section_value in sections.iter() {
-        let section: &HashMap<String, Value> = match section_value {
+    for section_value in sections {
+        let section: &mut HashMap<String, Value> = match section_value {
             Value::Compound(compound) => compound,
             _ => panic!("Failed to get section")
         };
@@ -31,8 +31,38 @@ fn main() {
             Value::Int(num) => num,
             _ => panic!("Failed to get Y value")
         };
-        println!("Section");
-        println!("Y position: {:?}", section_y);
+
+        if section_y != -4 {
+            continue;
+        }
+
+        let block_states: &mut HashMap<String, Value> = match section.get_mut("block_states").unwrap() {
+            Value::Compound(compound) => compound,
+            _ => panic!("Failed to get block states")
+        };
+
+        let palette: &mut Vec<Value> = match block_states.get_mut("palette").unwrap() {
+            Value::List(list) => list,
+            _ => panic!("Failed to get palette")
+        };
+
+        for block_value in palette {
+            let block: &mut HashMap<String, Value> = match block_value {
+                Value::Compound(compound) => compound,
+                _ => panic!("Failed to get block")
+            };
+
+            let name: &mut String = match block.get_mut("Name").unwrap() {
+                Value::String(string) => string,
+                _ => panic!("Failed to get block name")
+            };
+
+            if name == "minecraft:bedrock" {
+                name.replace_range(.., "minecraft:dirt");
+            }
+        }
+
+        println!("{:?}", section);
     }
 
     let bytes = to_bytes(&chunk).unwrap();
