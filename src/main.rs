d0c79067666dd44db7893c91288946391c9f2750
griffin-lora <36275551+griffin-lora@users.noreply.github.com>
2023-12-21 12:08:57 -0500

    read and write back

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use fastanvil::{CurrentJavaChunk, Region, Chunk, Block};
+use fastanvil::{CurrentJavaChunk, Region, Chunk, Block, Result};
+use fastnbt::error::Error;
 use fastnbt::{Value};
 use fastnbt::from_bytes;
 use fastnbt::to_bytes;
+use std::fs::File;
 
 //
 // This loads a region file, extracts a chunk from it, and uses serde to
@@ -9,14 +11,17 @@ use fastnbt::to_bytes;
 //
 
 fn main() {
-    let mut file: std::fs::File = std::fs::File::open("regions/vanilla.mca").unwrap();
+    let file: File = File::options().read(true).write(true).open("regions/vanilla.mca").unwrap();
 
-    let mut region: Region<std::fs::File> = Region::from_stream(file).unwrap();
+    let mut region: Region<File> = Region::from_stream(file).unwrap();
     let data = region.read_chunk(0, 0).unwrap().unwrap();
 
     let chunk: Value = from_bytes(data.as_slice()).unwrap();
 
     let bytes = to_bytes(&chunk).unwrap();
 
-    region.write_chunk(0, 0, &bytes);
+    match region.write_chunk(0, 0, &bytes) {
+        Ok(_) => {}
+        Err(error) => panic!("Error writing chunk: {:?}", error)
+    };
 }
