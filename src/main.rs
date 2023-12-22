1784f74fa9dcd72a29c5099210efeadf71cfacc0
griffin-lora <36275551+griffin-lora@users.noreply.github.com>
2023-12-21 19:58:04 -0500

    add proper file io

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,21 @@ mod plopper;
 use plopper::plop_region;
 
 fn main() {
-    plop_region("regions/upper.mca", "regions/lower.mca");
+    let x_lower_bound = -1;
+    let x_upper_bound = 1;
+    let z_lower_bound = -1;
+    let z_upper_bound = 1;
+
+    for z in z_lower_bound..(z_upper_bound + 1) {
+        for x in x_lower_bound..(x_upper_bound + 1) {
+            let upper_path = format!("regions/upper/r.{:?}.{:?}.mca", x, z);
+            let lower_path = format!("regions/lower/r.{:?}.{:?}.mca", x, z);
+
+            println!("Begin plopping r.{:?}.{:?}.mca", x, z);
+            
+            plop_region(&upper_path, &lower_path);
+
+            println!("End plopping r.{:?}.{:?}.mca", x, z);
+        }
+    }
 }
